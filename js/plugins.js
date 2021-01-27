18793b182fca87c091d2520c40a43779335439ae
lundeen-bryan <lundeen.bryan@gmail.com>
2021-01-27 09:41:03 -0800

    Added searchbox using selectpicker

--- js/plugins.js
+++ js/plugins.js
@@ -1,7 +1,31 @@
 /*========== NAVBAR TRANSPARENT TO SOLID ==========*/
+$(document).ready(function () { //when document(DOM) loads completely
+    checkScroll(); //check if page is scrolled
+    $(window).scroll(checkScroll); //get scroll position of window
+});
+
+function checkScroll() { //check if page is scrolled
+if ($(window).scrollTop() >= 300) { //if window is scrolled 300px or more
+    $('.navbar').addClass('solid'); //add class 'solid' to element with class 'navbar'
+} else { //if page is not scrolled 300px from top
+    $('.navbar').removeClass('solid'); //remove class 'solid' from navbar element
+}
+}
 
 
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
+$(document).ready(function () {
+	checkScroll();
+	$(window).scroll(checkScroll);
+	$('.navbar-toggler').click(function () {
+		if (!$('.navbar').hasClass('solid-toggle')) {
+			if ($(window).scrollTop() < 300) {
+				$('.navbar').toggleClass('solid-toggle');
+			}
+		}
+	});
+});
+
 
 
 /*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK ==========*/
